76f6728615d5ff2459d0a393f57795466e3a7373
unknown <moniquejroliveira@gmail.com>
2020-07-07 09:06:19 -0300

    média de um aluno

---
+++ ex07.py
@@ -0,0 +1,5 @@
+print('Vamos calcular a média de um aluno?')
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+media = (n1 + n2) / 2
+print('A média do aluno, com as notas {} e {}, é de {}.'.format(n1, n2, media))
