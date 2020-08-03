ab3f9d0dbe237ec23d0b533d13aa68d231cbff86
unknown <moniquejroliveira@gmail.com>
2020-08-03 14:19:12 -0300

    Adivinhação de número par ou ímpar

---
+++ ex030.py
@@ -0,0 +1,6 @@
+num = int(input('Digite um número qualquer, inteiro: '))
+resp = num % 2
+if resp == 0:
+    print('O número {} é PAR!'.format(num))
+else:
+    print('O número {} é ÍMPAR!'.format(num))
